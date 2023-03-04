0ea556ab71d159870342d81cabc58dbcb9bc439c
12116809 <lestianm@gmail.com>
2023-03-03 21:21:56 -0300

    implementa construtor equals e hashcode

--- src/br/newtonpaiva/dominio/Conta.java
+++ src/br/newtonpaiva/dominio/Conta.java
@@ -1,8 +1,26 @@
 package br.newtonpaiva.dominio;
 
+import java.util.Objects;
+
 public class Conta {
     private Integer numero;
     private Double saldo;
+
+    //construtores
+    public Conta(){
+        this(null);
+    }
+
+    public Conta(Integer numero){
+//        this.numero = numero;
+//        this.saldo = 0.0;
+        this(numero, 0.0);
+    }
+
+    public Conta(Integer numero, Double saldo){
+        this.numero = numero;
+        this.saldo = saldo;
+    }
 
     public Double sacar (Double valor){
         return 0.0;
@@ -33,5 +51,20 @@ public class Conta {
 
     public void setSaldo(Double saldo){
         this.saldo = saldo;
+    }
+    //comparação de uma classe conta com outra calsse conta do sistema
+
+//alt + insert -> equals() and hashCode()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return Objects.equals(numero, conta.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
     }
 }
--- src/br/newtonpaiva/ui/Main.java
+++ src/br/newtonpaiva/ui/Main.java
@@ -4,20 +4,30 @@ import br.newtonpaiva.dominio.Conta;
 
 public class Main {
     public static void main(String[] args) {
-       Conta c = new Conta();
-       c.setNumero(10);
+       Conta c = new Conta(200);
+//       c.setNumero(10);
        c.setSaldo(100.0);
 
         c.depositar(50.0);
         //c.depositar(-20.0);
 
         System.out.println("O valor do seu saldo é : " + c.getSaldo());
-        System.out.println("Número : " + c.getNumero());
+        System.out.println("O número da sua conta é : " + c.getNumero());
 
-        c.setNumero(9);
-        c.setSaldo(50.0);
+        Conta c2 = new Conta(300, 500.0);
+//        c2.setNumero(90);
+//        c2.setSaldo(500.0);
 
-        System.out.println("O valor do seu saldo é : " + c.getSaldo());
-        System.out.println("Número : " + c.getNumero());
+        System.out.println("O valor do seu saldo é : " + c2.getSaldo());
+        System.out.println("O número da sua conta é: " + c2.getNumero());
+
+        Conta c3 = new Conta();
+
+//        if(c.equals(c2))
+//            System.out.println("\nMesma conta");
+//        else
+//            System.out.println("\nContas diferentes");
+
+        System.out.println((c.equals(c2)) ? ("\nMesma conta") : ("\nContas diferentes"));
     }
 }
